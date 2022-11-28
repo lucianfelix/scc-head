250f85eda607c54a3af5c2f1f2a9caada3a9d72f
Lucian Felix Ghita <lucianfelix@gmail.com>
2022-11-28 18:28:35 +0100

    support for custom blocks

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,36 @@
-import { load } from '../lib/data';
+import {load} from '../lib/data';
 import Link from 'next/link';
+
+import {parseSlideshow, parseArticles, parseLinkList} from '../lib/upm.js';
 
 export default async function Page() {
 
-  const data = await load();
-  return (
-    <div className="space-y-8">
-      <h1 className="text-xl font-medium text-gray-300">Examples</h1>
+    const data = await load(
+        "https://main--upm--hlxsites.hlx.live",
 
-        <div><pre>{JSON.stringify(data, null, 2) }</pre></div>
+        [
+            {
+                blockType: "slideshow",
+                parser: parseSlideshow
+            },
+            {
+                blockType: "article",
+                parser: parseArticles
+            },
+            {
+                blockType: "link-list",
+                parser: parseLinkList
+            },
+        ]
+    );
+    return (
+        <div className="space-y-8">
+            <h1 className="text-xl font-medium text-gray-300">Examples</h1>
 
-    </div>
-  );
+            <div>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+            </div>
+
+        </div>
+    );
 }
