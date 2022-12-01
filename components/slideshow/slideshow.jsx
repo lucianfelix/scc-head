c73d05a6430078344f15e34458da13475ac5cea0
Duy Nguyen <dunguyen@adobe.com>
2022-12-01 11:11:56 +0100

    use client nav link in slideshow

--- components/slideshow/slideshow.jsx
+++ components/slideshow/slideshow.jsx
@@ -3,6 +3,7 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './slideshow.css'
 import Image from "next/image";
+import Link from "next/link";
 import CarouselWrapper from "./CarouselWrapper";
 
 export default function Slideshow({data}) {
@@ -14,11 +15,11 @@ export default function Slideshow({data}) {
           if(link.href.charAt(0) === '/') link.href = `upm${link.href}`
           return (
             <div key={`slide_${i + 1}`}>
-              <a href={link.href} title={link.text}>
+              <Link href={link.href} title={link.text}>
                 <Image
                     src={pictureUrl} width={width} height={height} alt={link.text}
                     loading={i === 0 ? 'eager' : 'lazy'} priority={i === 0} />
-              </a>
+              </Link>
             </div>
           );
         }
