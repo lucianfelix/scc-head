403da5c82b8eb213745fc6c34b3501a3cadcdf01
Ghita Lucian <ghita@adobe.com>
2022-12-01 17:11:35 +0100

    fix preload link

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ export default function RootLayout({
                 fetchpriority="high"
                 media="(max-width: 840px)"
                 as="image"
-                href="http://localhost:3000/_next/image?url=https%3A%2F%2Fmain--upm--hlxsites.hlx.live%2Fmedia_1b0a9abb5fd97958342f83943585198d71d7cd0bf.png%3Fwidth%3D750%26format%3Dpng%26optimize%3Dmedium&w=3840&q=75"
+                href="./_next/image?url=https%3A%2F%2Fmain--upm--hlxsites.hlx.live%2Fmedia_1b0a9abb5fd97958342f83943585198d71d7cd0bf.png%3Fwidth%3D750%26format%3Dpng%26optimize%3Dmedium&w=3840&q=75"
               // imageSizes="50vw"
           />
       </head>
