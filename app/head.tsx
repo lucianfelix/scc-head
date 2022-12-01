179b835612c5b25695b9b57ae03cedfa3c9d72d7
Ghita Lucian <ghita@adobe.com>
2022-12-01 14:48:08 +0100

    tweaks

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
-  return (
-    <>
-      <title>Root</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  );
+    return (
+        <>
+            <meta name="description" content="RNA Experiments."/>
+            <meta content="width=device-width, initial-scale=1" name="viewport"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </>
+    );
 }
--- app/upm/head.tsx
+++ app/upm/head.tsx
@@ -1,9 +1,10 @@
 export default function Head() {
-  return (
-    <>
-      <title>UPM</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  );
+    return (
+        <>
+            <title>UPM</title>
+            <meta name="description" content="UMP RNA Experiments."/>
+            <meta content="width=device-width, initial-scale=1" name="viewport"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </>
+    );
 }
