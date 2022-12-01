f4eb2032a2bd70ade0da1e3c6638bed3914430ed
Ghita Lucian <ghita@adobe.com>
2022-12-01 10:34:26 +0100

    carousel uses Next/Image for images, eager first

--- components/slideshow/slideshow.jsx
+++ components/slideshow/slideshow.jsx
@@ -4,8 +4,10 @@ import { Carousel } from 'react-responsive-carousel';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './slideshow.css'
+import Image from "next/image";
 
 export default function Slideshow({data}) {
+  const num_slides = data.slides.length;
 
   return (<>
     <Carousel
@@ -14,15 +16,24 @@ export default function Slideshow({data}) {
         interval={5000}
         showThumbs={false}>
       {data.slides.map(
-        ({ type, picture, link }, i) => {
-          const pictureHtml = picture.replaceAll('./media', 'https://main--upm--hlxsites.hlx.live/media')
+        ({ type, picture, pictureUrl, width, height, link }, i) => {
+
+          // let url = pictureUrl;
+          pictureUrl = pictureUrl.replaceAll('./media', 'https://main--upm--hlxsites.hlx.live/media')
           if(link.href.charAt(0) === '/') link.href = `upm${link.href}`
-          const num_slides = data.slides.length
+
           return (
             <div key={`slide_${i + 1}`}>
-              <a href={link.href} title={link.text} disabled>
-                <div dangerouslySetInnerHTML={{__html: pictureHtml}} />
-              </a>
+              <Image
+                  src={pictureUrl}
+                  width={width}
+                  height={height}
+                  alt={link.text}
+                  loading={i === 0 ? 'eager' : 'lazy'}
+                  priority={i === 0} />
+              {/*<a href={link.href} title={link.text} disabled>*/}
+              {/*  <div dangerouslySetInnerHTML={{__html: pictureHtml}} />*/}
+              {/*</a>*/}
             </div>
           );
         }
